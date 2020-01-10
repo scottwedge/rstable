a14161546d7c3777a99ea11d7b87e6c53bf610b2
nstremmel <noahstremmel22@gmail.com>
2020-01-10 17:38:24 -0600

    fixes

--- rstable.py
+++ rstable.py
@@ -12,6 +12,7 @@ from utilities import isstaff, formatok, formatfromk, pickflower, scorefp
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 c=conn.cursor()
+conn.setAutoCommit(True);
 
 # c.execute("DROP TABLE rsmoney")
 # c.execute("""CREATE TABLE rsmoney (
@@ -446,7 +447,7 @@ async def on_message(message):
 		if message.author.id!="580511336598077511":
 			await client.delete_message(message)
 			embed = discord.Embed(description=str(message.content).title(), color=15925108)
-			embed.set_author(name="Suggestion From "+str(message.author), icon_url=str(message.server.icon_url))
+			embed.set_author(name="Suggestion From "+str(message.author)[:-5], icon_url=str(message.server.icon_url))
 			embed.set_footer(text="Suggested On:"+str(datetime.datetime.now())[:-7])
 			embed.set_footer(text="👍 = Like | 👎 = Dislike")
 			sent=await client.send_message(message.channel, embed=embed)
