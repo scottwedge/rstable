91d6b51c18e204d2acd3e811fa4e4af9b0abf224
nstremmel <noahstremmel22@gmail.com>
2020-01-12 18:49:57 -0600

    Im confused

--- rstable.py
+++ rstable.py
@@ -1332,12 +1332,12 @@ async def on_message(message):
 				total=sum(x[1] for x in bets)
 				embed = discord.Embed(description='Jackpot Value: **'+formatfromk(total, '07')+'**\nUse `$add (amount in 07)` to contribute to the jackpot.', color=5056466)
 
+				print(bets)
 				for i in bets:
 					print(i[0])
-					test=message.server.get_member(i[0])
 					chance=round(i[1]/total*100, 3)
 					c.execute('UPDATE jackpot SET chance={} WHERE id={}'.format(float(chance), int(i[0])))
-					embed.add_field(name=(message.server.get_member(str(i[0]))).name, value='Bet - *'+formatfromk(i[1], '07')+'* | Chance of Winning - *'+str(chance)+'%*', inline=False)
+					embed.add_field(name=(message.server.get_member(i[0])).name, value='Bet - *'+formatfromk(i[1], '07')+'* | Chance of Winning - *'+str(chance)+'%*', inline=False)
 				embed.set_author(name="Jackpot Bets", icon_url=str(message.server.icon_url))
 				embed.set_footer(text='*You can only bet 07 gold on the Jackpot game')
 				await client.send_message(message.channel, embed=embed)
