0e958b6688ffd7a6a9556f8f198fd7a005ad576b
nstremmel <noahstremmel22@gmail.com>
2020-02-12 17:26:34 -0600

    fixes

--- rstable.py
+++ rstable.py
@@ -1312,7 +1312,7 @@ async def on_message(message):
 		minutes = 60-int(difference.seconds / 60)
 
 		if bronze in message.author.roles or silver in message.author.roles or gold in message.author.roles:
-			if days>=7:
+			if days<=-1:
 				if bronze in message.author.roles:
 					bkeys=getvalue(int(message.author.id),'bronze','rsmoney')
 					c.execute('UPDATE rsmoney SET bronze={} WHERE id={}'.format(bkeys+5, message.author.id))
