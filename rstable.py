01cf8c7ecb6236c520a361bb13f157545f09e6aa
nstremmel <noahstremmel22@gmail.com>
2020-03-09 22:54:29 -0500

    'fixes

--- rstable.py
+++ rstable.py
@@ -1509,7 +1509,7 @@ async def on_message(message):
 		embed = discord.Embed(description=
 					"Level: **" + str(level) + "**\n" +
 					"Total XP: **" + str(xp) + "**\n" +
-					"XP Until Next Level: **" + str(500*((level+1)**2)+1000-xp) + "**\n" +
+					"XP Until Level " + str(level+1) + ": **" + str(500*(level**2)+1000-xp) + "**\n" +
 					"Rank: **#"+ str(rank) + "** of **" + str(len(leaderboard)) + "**", color=7995152)
 		embed.set_author(name=(str(message.author))[:-5]+"'s Levels", icon_url=str(message.server.icon_url))
 		embed.set_thumbnail(url=str(message.author.avatar_url))
