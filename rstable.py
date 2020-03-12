f3f524a8ca650c0e5e30421b6cf08c529522b406
nstremmel <noahstremmel22@gmail.com>
2020-03-11 21:51:17 -0500

    tests

--- rstable.py
+++ rstable.py
@@ -1523,7 +1523,7 @@ async def on_message(message):
 		embed.set_author(name=(str(message.author))[:-5]+"'s Levels", icon_url=str(message.server.icon_url))
 		embed.set_thumbnail(url=str(message.author.avatar_url))
 		embed.set_footer(text="Send messages to level up!")
-		await client.send_message(message.channel, file=template, embed=embed)
+		await client.send_message(message.channel, template, embed=embed)
 
 
 client.loop.create_task(my_background_task())
